d993964a36490784180d894f540474d6b39215c3
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-05-01 15:33:43 +0100

    Added setup modal and successfully able to store player names and markers in local storage

--- src/displayController.js
+++ src/displayController.js
@@ -15,26 +15,65 @@ const generateGrid = () => {
         gridContainer.appendChild(gridDiv);
     }
 
-    /*
+    
     gridContainer.addEventListener("click", (e) => {
         const marker = document.createElement('p');
         marker.textContent = 'X';
 
         console.log(e.target.textContent)
         e.target.appendChild(marker);
-    })*/
+    })
 }
 
 const setupModal = () => {
     const myForm = document.getElementById('modalForm');
+    const player1markerBtns = document.querySelectorAll('.player1-markers')
+    const player2markerBtns = document.querySelectorAll('.player2-markers')
+
+    let player1marker = '', player2marker = '';
+
+    player1markerBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            player1marker = btn.value;
+
+            if (player1marker == 'X') {
+                player1markerBtns[0].classList.add('active');
+                player1markerBtns[1].classList.remove('active');
+                player2markerBtns[0].disabled = true;
+                player2markerBtns[1].disabled = false;
+            } else if (player1marker == 'O') {
+                player1markerBtns[1].classList.add('active');
+                player1markerBtns[0].classList.remove('active');
+                player2markerBtns[0].disabled = false;
+                player2markerBtns[1].disabled = true;
+            }
+        })
+    })
+
+    player2markerBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            player2marker = btn.value;
+
+            if (player2marker == 'X') {
+                player2markerBtns[0].classList.add('active');
+                player2markerBtns[1].classList.remove('active');
+            } else if (player2marker == 'O') {
+                player2markerBtns[1].classList.add('active');
+                player2markerBtns[0].classList.remove('active');
+            }
+        })
+    })
 
     myForm.addEventListener("submit", (e) => {
         e.preventDefault();
+        
+        localStorage.setItem('player1-name', document.getElementById('name1').value);
+        localStorage.setItem('player1-marker', player1marker);
+        localStorage.setItem('player2-name', document.getElementById('name2').value);
+        localStorage.setItem('player2-marker', player2marker);
 
-        const player1Name = document.getElementById('name1').value;
-        const player2Name = document.getElementById('name2').value;
-        console.log(player1Name);
-        console.log(player2Name);
+        const modalContainer = document.querySelector('.setup-modal-container');
+        modalContainer.classList.remove('active');
     })
 }
 export { generateUI }
